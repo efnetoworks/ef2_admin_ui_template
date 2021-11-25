f88e46d02165204b39426700d7d23b94066dab5b
Ibidapo Adeolu <bhekor@users.noreply.github.com>
2021-11-25 16:55:37 +0100

    deleted the nuxt

--- plugins/vuelidate.client.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import Vuelidate from 'vuelidate'
-import { required, minLength, between } from 'vuelidate/lib/validators'
-
-Vue.use(Vuelidate)
--- plugins/vuestepwizard.client.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-import VueStepWizard from "vue-step-wizard";
-import "vue-step-wizard/dist/vue-step-wizard.css";
-
-Vue.use(VueStepWizard);
